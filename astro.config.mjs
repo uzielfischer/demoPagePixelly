060416871ac44511c19a6df4227066df382fd845
UZIEL FISCHER <57245955+uzielfischer@users.noreply.github.com>
2025-02-24 12:44:53 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://pixelly.github.io",
   output: "server",
   adapter: netlify()
 });
